cdca4da337f3ad5152d20dee04c171f0cf6b8933
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-04-30 21:15:38 +0800

    check if inquiry set value is not 0

--- templates/single-lesson-plans.php
+++ templates/single-lesson-plans.php
@@ -378,7 +378,7 @@ if (have_posts()) : while (have_posts()) : the_post();
                 ?>
                 <div style="display:none"><?php var_dump($inquiry_set); ?></div>
                 <?php
-                if ($inquiry_set!==0) {
+                if ($inquiry_set!=="0") {
                     $inquiry = oer_lp_get_inquiry_set_details($inquiry_set);
                     $inquiry_link = get_permalink($inquiry_set);
                     $inquiry_img = get_the_post_thumbnail_url($inquiry);
