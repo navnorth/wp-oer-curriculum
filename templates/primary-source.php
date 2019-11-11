57a99a8406c3e17430245571cec5d081056cec59
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-11-11 20:44:17 +0800

    update related primary sources display

--- templates/primary-source.php
+++ templates/primary-source.php
@@ -7,16 +7,12 @@ add_filter('body_class', function($classes){
 
 get_header();
 
-$active_tab = null;
 $back_url = "";
 $source_id = 0;
 $lp_prev_class = "";
 $lp_next_class = "";
 $prev_url = "";
 $next_url = "";
-
-if (isset($_POST['activeTab']))
-    $active_tab = $_POST['activeTab'];
 
 // Back Button URL
 $curriculum = get_query_var('curriculum');
@@ -80,78 +76,98 @@ if (function_exists('oer_get_resource_metadata')){
 }
 ?>
 <div class="lp-nav-block"><a class="back-button" href="<?php echo $back_url; ?>"><i class="fas fa-arrow-left"></i><?php echo $curriculum_details->post_title; ?></a></div>
-<div class="ps-media-image col-md-4 col-sm-12" data-curid="<?php echo $index; ?>">
-    <?php if ($youtube): ?>
-    <div class="ps-youtube-video">
-        <?php
-            echo '<div class="youtubeVideoWrapper">';
-            if (function_exists('oer_generate_youtube_embed_code'))
-                $embed = oer_generate_youtube_embed_code($resource_url);
-            echo $embed;
-            echo '</div>';
-        ?>
-    </div>
-    <?php elseif ($isPDF): ?>
-    <div class="ps-pdf-block">
-        <?php
-            echo '<div class="psPDFWrapper">';
-            oer_display_pdf_embeds($resource_url);
-            echo '</div>';
-        ?>
-    </div>
-    <?php else: ?>
-    <div class="ps-image-block">
-       <img src="<?php echo $featured_image_url; ?>" alt="<?php echo $resource->post_title; ?>" />
-    </div>
-    <?php endif; ?>
-    <span class="ps-expand"><a href="<?php echo $featured_image_url; ?>" class="lp-expand-img" target="_blank"><i class="fas fa-external-link-alt"></i></a></span>
-    <div class="lp-center">
-        <?php if (isset($resource_meta['oer_resourceurl'])) { ?>
-        <div class="ps-meta-group ps-resource-url">
-            <a href="<?php echo $resource_meta['oer_resourceurl'][0]; ?>" class="tc-view-button" target="_blank"><?php _e("View Original", OER_LESSON_PLAN_SLUG); ?></a>
+<div class="row ps-details-row">
+    <div class="ps-media-image col-md-4 col-sm-12" data-curid="<?php echo $index; ?>">
+        <?php if ($youtube): ?>
+        <div class="ps-youtube-video">
+            <?php
+                echo '<div class="youtubeVideoWrapper">';
+                if (function_exists('oer_generate_youtube_embed_code'))
+                    $embed = oer_generate_youtube_embed_code($resource_url);
+                echo $embed;
+                echo '</div>';
+            ?>
         </div>
-        <?php } ?>
+        <?php elseif ($isPDF): ?>
+        <div class="ps-pdf-block">
+            <?php
+                echo '<div class="psPDFWrapper">';
+                oer_display_pdf_embeds($resource_url);
+                echo '</div>';
+            ?>
+        </div>
+        <?php else: ?>
+        <div class="ps-image-block">
+           <img src="<?php echo $featured_image_url; ?>" alt="<?php echo $resource->post_title; ?>" />
+        </div>
+        <?php endif; ?>
+        <span class="ps-expand"><a href="<?php echo $featured_image_url; ?>" class="lp-expand-img" target="_blank"><i class="fas fa-external-link-alt"></i></a></span>
+        <div class="lp-center">
+            <?php if (isset($resource_meta['oer_resourceurl'])) { ?>
+            <div class="ps-meta-group ps-resource-url">
+                <a href="<?php echo $resource_meta['oer_resourceurl'][0]; ?>" class="tc-view-button" target="_blank"><?php _e("View Original", OER_LESSON_PLAN_SLUG); ?></a>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+    <?php
+    $resource_meta = null;
+    $subject_areas = null;
+    ?>
+    <div class="ps-details col-md-8 col-sm-12">
+        <div class="ps-info">
+            <h1 class="ps-info-title"><?php echo $resource->post_title; ?></h1>
+            <div class="ps-info-description">
+                <?php echo $resource->post_content; ?>
+            </div>
+        </div>
     </div>
 </div>
-<?php
-$resource_meta = null;
-$subject_areas = null;
-$transcription_display = false;
-$sensitive_material_display = false;
-$tab_count = 3;
-if (isset($resource_meta['oer_transcription']) && $resource_meta['oer_transcription'][0]!==""){
-    $transcription_display = true;
-    $tab_count++;
-}
-
-if (isset($resource_meta['oer_sensitive_material']) && $resource_meta['oer_sensitive_material'][0]!==""){
-    $sensitive_material_display = true;
-    $tab_count++;
-}
-$tabs = floor(12/$tab_count);   
-
-if (in_array($active_tab,array("ps-transcription-info-tab","ps-sensitive-info-tab"))){
-    if (($active_tab=="ps-transcription-info-tab" && !$transcription_display) || ($active_tab=="ps-sensitive-info-tab" && !$sensitive_material_display))
-        $active_tab = null;
-}
-?>
-<div class="ps-details col-md-8 col-sm-12">
-<?php if ($sensitive_material_display==true) : ?>
-<div class="tc-sensitive-material-section tc-primary-source-sensitive-material-section">
-    <p><i class="fal fa-exclamation-triangle"></i><span class="sensitive-material-text">Sensitive Material</span></p>
-    <button class="question-popup-button" role="button" data-toggle="tab" data-tabid="ps-sensitive-info-tab" href="#ps-sensitive-info-tab-content"><i class="fal fa-question-circle"></i></button>
-</div>
-<?php endif; ?>
-<div class="ps-info">
-    <h1 class="ps-info-title"><?php echo $resource->post_title; ?></h1>
-    <div class="ps-info-description">
-        <?php echo $resource->post_content; ?>
+<div class="ps-related-sources lp-primary-sources-row">
+    <div class="lp-ps-nav-left-block <?php echo $lp_prev_class; ?> col-md-6 col-sm-12">
+        <?php if (!empty($prev_resource)):
+        $resource_img = wp_get_attachment_image_url( get_post_thumbnail_id($prev_resource), 'resource-thumbnail' );
+        ?>
+        <a class="lp-ps-nav-left" href="<?php echo $prev_url; ?>" data-activetab="" data-id="<?php echo $index-1; ?>" data-count="<?php echo count($primary_resources['resource']); ?>" data-curriculum="<?php echo $curriculum_id; ?>" data-prevsource="<?php echo $primary_resources['resource'][$index-1]; ?>">
+            <span class="col-md-3">&nbsp;</span>
+            <span class="nav-media-icon"><i class="fas fa-arrow-left fa-2x"></i></span>
+            <span class="nav-media-image col-md-8">
+                <span class="nav-image-thumbnail col-md-4">
+                    <?php if ($resource_img!==""):
+                    $ps_url = site_url("inquiry-sets/".sanitize_title($post->post_name)."/source/".sanitize_title($prev_resource->post_title)."-".$resource->ID);
+                    ?>
+                    <div class="resource-thumbnail" style="background: url('<?php echo $resource_img ?>') no-repeat center rgba(204,97,12,.1); background-size:cover;"></div>
+                    <?php endif; ?>
+                </span>
+                <span class="nav-lp-resource-title col-md-8">
+                    <?php echo $prev_resource->post_title; ?>
+                </span>
+            </span>
+        </a>
+        <?php endif; ?>
     </div>
-</div>
-</div>
-<div class="ps-related-sources">
-    <span class="ps-nav-left <?php echo $lp_prev_class; ?>"><a class="lp-nav-left" href="<?php echo $prev_url; ?>" data-activetab="" data-id="<?php echo $index-1; ?>" data-count="<?php echo count($primary_resources['resource']); ?>" data-curriculum="<?php echo $curriculum_id; ?>" data-prevsource="<?php echo $primary_resources['resource'][$index-1]; ?>"><i class="fal fa-chevron-left fa-2x"></i></a></span>
-    <span class="ps-nav-right <?php echo $lp_next_class; ?>"><a class="lp-nav-right" href="<?php echo $next_url; ?>" data-activetab="" data-id="<?php echo $index+1; ?>" data-count="<?php echo count($primary_resources['resource']); ?>" data-curriculum="<?php echo $curriculum_id; ?>" data-nextsource="<?php echo $primary_resources['resource'][$index+1]; ?>"><i class="fal fa-chevron-right fa-2x"></i></a></span>
+    <div class="lp-ps-nav-right-block <?php echo $lp_next_class; ?> col-md-6 col-sm-12">
+        <?php if (!empty($next_resource)):
+        $resource_img = wp_get_attachment_image_url( get_post_thumbnail_id($prev_resource), 'resource-thumbnail' );
+        ?>
+        <a class="lp-ps-nav-right" href="<?php echo $next_url; ?>" data-activetab="" data-id="<?php echo $index+1; ?>" data-count="<?php echo count($primary_resources['resource']); ?>" data-curriculum="<?php echo $curriculum_id; ?>" data-nextsource="<?php echo $primary_resources['resource'][$index+1]; ?>">
+            <span class="nav-media-image col-md-8">
+                <span class="nav-image-thumbnail col-md-4">
+                    <?php if ($resource_img!==""):
+                    $ps_url = site_url("inquiry-sets/".sanitize_title($post->post_name)."/source/".sanitize_title($resource->post_title)."-".$resource->ID);
+                    ?>
+                    <div class="resource-thumbnail" style="background: url('<?php echo $resource_img ?>') no-repeat center rgba(204,97,12,.1); background-size:cover;"></div>
+                    <?php endif; ?>
+                </span>
+                <span class="nav-lp-resource-title col-md-8">
+                    <?php echo $next_resource->post_title; ?>
+                </span>
+            </span>
+            <span class="nav-media-icon"><i class="fas fa-arrow-right fa-2x"></i></span>
+            <span class="col-md-3">&nbsp;</span>
+        </a>
+        <?php endif; ?>
+    </div>
 </div>
 <div class="lp-ajax-loader" role="status">
     <div class="lp-ajax-loader-img">
