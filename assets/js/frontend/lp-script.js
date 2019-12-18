ef6fd6c5cc9d7d5190c20bcf9da6fdd6eecaa8f2
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-12-18 19:09:24 +0800

    adjust top position on going back to top

--- assets/js/frontend/lp-script.js
+++ assets/js/frontend/lp-script.js
@@ -13,9 +13,9 @@ jQuery(document).ready(function($){
                 });
             } else {
                 topPos = headerHeight - document.documentElement.scrollTop;
-                topPos += 40;
+                topPos += 60;
                 if (jQuery('#wpadminbar.mobile').length>0)
-                    topPos = topPos + 32;
+                    topPos = topPos + 46;
                 jQuery('.tc-lp-details-header').css({
                     "background":"none",
                     "padding":"25px 25px 0",
