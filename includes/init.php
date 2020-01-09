c10b0c4e7339d8cff71cda982f086d11e9769655
josepheneldas <39935891+josepheneldas@users.noreply.github.com>
2020-01-09 17:06:38 +0800

    Revert "Added quicktags in tinymce"
    This reverts commit 2175f23f298641ca0d92ffbd4f84109d0a6980ca.

--- includes/init.php
+++ includes/init.php
@@ -628,7 +628,6 @@ function lp_add_more_pr_callback() {
                                       'textarea_rows' => 6,
                                       'drag_drop_upload' => true,
                                       'teeny' => true,
-                                      'quicktags' => true
                                   )
                               );
                               $content .= ob_get_clean();
