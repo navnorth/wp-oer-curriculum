93da98acae7335fa9b1c60bcb7dc7160f1b992f2
josepheneldas <39935891+josepheneldas@users.noreply.github.com>
2020-01-09 22:36:42 +0800

    Followup

--- assets/js/backend/lesson-plan.js
+++ assets/js/backend/lesson-plan.js
@@ -803,8 +803,8 @@ jQuery(document).ready(function ($) {
                         tinymce.execCommand( 'mceAddEditor', false, 'oer-lp-resource-teacher-' + total_form_box );
                         quicktags({ id: 'oer-lp-resource-teacher-' + total_form_box });
                         
-                        tinymce.execCommand( 'mceRemoveEditor', false, 'oer-lp-resource-student-' + total_form_box );
-                        //tinymce.execCommand( 'mceAddEditor', false, 'oer-lp-resource-student-' + total_form_box );
+                        //tinymce.execCommand( 'mceRemoveEditor', false, 'oer-lp-resource-student-' + total_form_box );
+                        tinymce.execCommand( 'mceAddEditor', false, 'oer-lp-resource-student-' + total_form_box );
                         quicktags({ id: 'oer-lp-resource-student-' + total_form_box });
                         
                         $('#oer-lp-resource-student-'+total_form_box+'-html').trigger('click');
