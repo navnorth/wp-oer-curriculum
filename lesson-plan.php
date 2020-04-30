ab7115b2d7993d4e1b23a0c5988f149bec872820
Joseph Eneldas <joseph.eneldas@navigationnorth.com>
2020-04-30 21:04:39 +0800

    WP Curriculum
    (cherry picked from commit 0bbff3700eaf58132dfe73cbae0f892cceeb7534)

--- lesson-plan.php
+++ lesson-plan.php
@@ -42,6 +42,8 @@ define( 'OER_LESSON_PLAN_VERSION', '0.3.5' );
 include_once(OER_LESSON_PLAN_PATH.'includes/oer-lp-functions.php');
 include_once(OER_LESSON_PLAN_PATH.'includes/init.php');
 
+require_once(OER_LESSON_PLAN_PATH.'includes/blocks/curriculum_block/init.php');
+
 global $oer_lp_default_structure;
 global $oer_convert_info;
 global $oer_lp_deleted_fields;
