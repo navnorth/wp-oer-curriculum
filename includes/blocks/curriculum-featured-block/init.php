cea90b7e8a7a61c195b2c719941824c03fd80740
Joseph Eneldas <joseph.eneldas@navigationnorth.com>
2020-05-28 03:44:23 +0800

    Update init.php

--- includes/blocks/curriculum-featured-block/init.php
+++ includes/blocks/curriculum-featured-block/init.php
@@ -244,7 +244,7 @@ function curriculum_feat_tax_query(){
 			$args = array(
 				'posts_per_page' => -1,
 				'post_type' => $_posttype,
-				'post_status' => 'published',
+				'post_status' => 'publish',
 				'tax_query' => array(
 					array(
 						'taxonomy' => 'resource-subject-area',
