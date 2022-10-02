f37f28f5dff17f2d9ea24830786d8c570912eb02
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-10-03 00:04:28 +0800

    WP-287 Add save button to curriculum list block modal

--- includes/blocks/curriculum-block/build/index.asset.php
+++ includes/blocks/curriculum-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5440de7b8c28b9d5bd2b27753d43ac28');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '78c25a65cdff4677de5dea121809a861');
